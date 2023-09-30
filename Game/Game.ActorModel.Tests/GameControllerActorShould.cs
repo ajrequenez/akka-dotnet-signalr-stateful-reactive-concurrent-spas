45f9e2e2bb9c594dd649600f93e0945d3e51192d
= <jrequenez@hotmail.com>
2023-09-29 21:57:58 -0500

    ✨ feat: create PllayerCoordinatorActor - no tests yet, need more info from course

---
+++ Game/Game.ActorModel.Tests/GameControllerActorShould.cs
@@ -0,0 +1,8 @@
+﻿using Akka.TestKit.Xunit2;
+
+namespace Game.ActorModel.Tests
+{
+    internal class GameControllerActorShould : TestKit
+    {
+    }
+}
---
+++ Game/Game.ActorModel/Actors/GameControllerActor.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Actor;
+using Game.ActorModel.Messages;
+
+namespace Game.ActorModel.Actors
+{
+    public class GameControllerActor : ReceiveActor
+    {
+        private readonly Dictionary<string, IActorRef> _players;
+
+        public GameControllerActor()
+        {
+            _players = new Dictionary<string, IActorRef>();
+
+            Receive<JoinGameMessage>(onJoinGame);
+        }
+
+        private void onJoinGame(JoinGameMessage message)
+        {
+            var playerName = message.PlayerName;
+
+            if(!_players.ContainsKey(playerName))
+            {
+                IActorRef newPlayer = Context.ActorOf(PlayerActor.Props(playerName));
+                _players[playerName] = newPlayer;
+
+                foreach(var player in _players.Values)
+                {
+                    player.Tell(new RequestPlayerStatusMessage(), Sender);
+                }
+            }
+        }
+
+        public static Props Props() => Akka.Actor.Props.Create(()=> new GameControllerActor());
+    }
+}
