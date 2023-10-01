a9f67b152b8cc9c53c7207ccf0bcd7dcec09efd5
= <jrequenez@hotmail.com>
2023-10-01 12:55:33 -0500

    🚧 wip: adding ActorSystem to web app - not long term solution

---
+++ Game/Game.Web/Models/GameActorSystem.cs
@@ -0,0 +1,28 @@
+﻿using Akka.Actor;
+using Akka.Dispatch.SysMsg;
+using Game.ActorModel.Actors;
+
+namespace Game.Web.Models
+{
+    public static class GameActorSystem
+    {
+        private static ActorSystem? GameSystem;
+
+        public static void Create()
+        {
+            GameSystem = Akka.Actor.ActorSystem.Create("GameSystem");
+            
+            ActorReferences.GameController = GameSystem.ActorOf<GameControllerActor>();
+        }
+
+        public static void Shutdown()
+        {
+            GameSystem?.Terminate();
+        }
+
+        public static class ActorReferences
+        {
+            public static IActorRef? GameController { get; set; } 
+        }
+    }
+}
