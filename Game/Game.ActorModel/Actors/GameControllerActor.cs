be40e5b4d2c64d4e9f4ab565890acc370932fa55
= <jrequenez@hotmail.com>
2023-11-17 21:00:57 -0600

    🚧 wip: commit work in progress for laptop migration

--- Game/Game.ActorModel/Actors/GameControllerActor.cs
+++ Game/Game.ActorModel/Actors/GameControllerActor.cs
@@ -1,4 +1,5 @@
 ﻿using Akka.Actor;
+using Akka.Event;
 using Game.ActorModel.Messages;
 
 namespace Game.ActorModel.Actors
@@ -6,10 +7,12 @@ namespace Game.ActorModel.Actors
     public class GameControllerActor : ReceiveActor
     {
         private readonly Dictionary<string, IActorRef> _players;
+        private readonly ILoggingAdapter _log = Context.GetLogger();
 
         public GameControllerActor()
         {
             _players = new Dictionary<string, IActorRef>();
+            _log.Info("GameControllerActor created...");
 
             Receive<JoinGameMessage>(onJoinGame);
 
@@ -18,12 +21,15 @@ namespace Game.ActorModel.Actors
 
         private void onAttackPlayer(AttackPlayerMessage message) 
         {
+            _log.Info($"Attacking Player {0}", message.PlayerName);
             _players[message.PlayerName].Forward(message);
         }
 
         private void onJoinGame(JoinGameMessage message)
         {
             var playerName = message.PlayerName;
+
+            _log.Info($"New player joining game...{0}", playerName);
 
             if(!_players.ContainsKey(playerName))
             {
---
+++ Game/Game.State/Program.cs
@@ -0,0 +1,43 @@
+﻿using Akka.Actor;
+using Akka.Remote;
+using Akka.Hosting;
+using Game.ActorModel.Actors;
+using Game.ActorModel.Messages;
+using Microsoft.Extensions.Hosting;
+
+namespace Game.State
+{
+    internal class Program
+    {
+        //private static ActorSystem? ActorSystemInstance;
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Starting Actor System...");
+
+            //ActorSystemInstance = ActorSystem.Create("GameStateSystem");
+            //var gameController = ActorSystemInstance.ActorOf<GameControllerActor>("GameController");
+            //gameController.Tell(new RequestPlayerStatusMessage());
+
+            //Console.ReadLine();
+
+
+            var hostBuilder = new HostBuilder();
+
+            hostBuilder.ConfigureServices((context, services) =>
+            {
+                services.AddAkka("GameStateSystem", (builder, sp) =>
+                {
+                    builder
+                        .WithActors((system, registry, resolver) =>
+                        {
+                            var gameController = system.ActorOf(Props.Create(() => new GameControllerActor()), "game-controller");
+                            registry.Register<GameControllerActor>(gameController);
+                        });
+                });
+            });
+
+            var host = hostBuilder.Build();
+            await host.RunAsync();
+        }
+    }
+}
